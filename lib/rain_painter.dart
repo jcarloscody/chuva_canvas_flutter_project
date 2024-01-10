152764748f3742bcf55c8608ac54b5b49352d0b7
Josue Carlos <josuecarlosos2@gmail.com>
2024-01-10 00:10:23 -0300

    model: modelo da gotinha

--- lib/rain_painter.dart
+++ lib/rain_painter.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -32,4 +33,47 @@ class RainPainter extends CustomPainter {
     // TODO: implement shouldRepaint
     throw UnimplementedError();
   }
+}
+
+class Drop {
+  final double x;
+
+  /// variavel [_originY]que indica a chuva caindo
+  final double _originY;
+
+  ///a medida que for cainda vai aumentando a velocidade da variavel [_originalSpeed]
+  final double _originalSpeed;
+
+  ///
+  final int dropSize;
+
+  ///o [blur] dará a sensação de profundidade.
+  final double blur;
+
+  ///y[_currentY] e speed [_currentSpeed] atuais
+  double _currentY;
+  double _currentSpeed;
+  Drop({
+    required this.x,
+    required double originY,
+    required double originalSpeed,
+    required this.dropSize,
+    required this.blur,
+  })  : _currentSpeed = originalSpeed,
+        _originalSpeed = originalSpeed,
+        _currentY = originY,
+        _originY = originY;
+
+  void increment() {
+    _currentSpeed += 0.05;
+    _currentY += _currentSpeed;
+  }
+
+  void reset() {
+    _currentY = _originY;
+    _currentSpeed = _originalSpeed;
+  }
+
+  double get y => _currentY;
+  double get currentSpeed => _currentSpeed;
 }
